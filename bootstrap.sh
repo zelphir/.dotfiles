e011e073edd43d66ff39cd1da3b402baf104aa91
Roberto Manzella <zelphir@gmail.com>
2016-02-15 23:46:38 +0000

    Change fonts

--- bootstrap.sh
+++ bootstrap.sh
@@ -48,7 +48,9 @@ e_success "Done!"
 ################################################################################
 
 e_running "Installing fixed-width fonts patched for use with Powerline symbols..."
-cd ~/Library/Fonts ; curl -fLo "Inconsolata for Powerline Nerd Font Complete Mono.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Inconsolata/complete/Inconsolata%20for%20Powerline%20Nerd%20Font%20Complete%20Mono.otf
+cd ~/Library/Fonts ; curl -fLo "Meslo LG M Regular for Powerline Nerd Font Complete Mono.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Meslo/M/complete/Meslo%20LG%20M%20Regular%20for%20Powerline%20Nerd%20Font%20Complete%20Mono.otf
+cd ~/Library/Fonts ; curl -fLo "Meslo LG S DZ Regular for Powerline Nerd Font Plus Octicons.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Meslo/S-DZ/additional-variations/Meslo%20LG%20S%20DZ%20Regular%20for%20Powerline%20Nerd%20Font%20Plus%20Octicons.otf
+
 e_success "Done!"
 
 ################################################################################
